ba9c6630dd70016e1a3ce451d2003f506493855f
BianconiFilippo <filippob2002@gmail.com>
2024-09-17 19:51:59 -0300

    adivinar letra && agregado de variables letras e intentos restantes

--- ahorcado-agiles/src/components/ahorcado.js
+++ ahorcado-agiles/src/components/ahorcado.js
@@ -1,12 +1,32 @@
 
 
-export default class Ahorcado{
-    constructor(palabra_secreta){
-        let palabra_secreta;
+export default class Ahorcado {
+    palabra_secreta;
+    intentos_restantes = 5;
+    letras = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, k, q, r, s, t, u, v, w, x, y, z];
+
+
+    constructor(palabra_secreta) {
+        this.palabra_secreta = palabra_secreta
+    }
+
+    adivinar_letra(letra) {
+        if (this.palabra_secreta.includes(letra)) {
+            this.letras.filter((element) => element !== letra);
+            return true
+        } else {
+            this.letras.filter((element) => element !== letra);
+            if (this.intentos_restantes > 1) {
+                this.intentos_restantes -= 1;
+            } else {
+                return false;
+            }
+            return true;
+        }
     }
 
     adivinar(palabra) {
-        return palabra==this.palabra_secreta
+        return palabra == this.palabra_secreta
     }
 
 }
--- ahorcado-agiles/src/components/ahorcado.spec.js
+++ ahorcado-agiles/src/components/ahorcado.spec.js
@@ -1,6 +1,8 @@
 import { expect, test } from "vitest";
 import { Ahorcado } from "./ahorcado";
 
-const ahc = new Ahorcado("Dia")
+const ahc = new Ahorcado("dia")
 
-test("Adivinar 'Dia'", () => expect(ahc.adivinar("Dia")).toBe(true));
+test("Adivinar 'dia'", () => expect(ahc.adivinar("dia")).toBe(true));
+
+test('Validar que una la letra este en la palabra', () => expect(ahc.adivinar("d")).toBe(true));
