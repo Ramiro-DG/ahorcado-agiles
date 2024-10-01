fcde2ca2be4489c54d7c8cdab3f6a476f165ffc1
BianconiFilippo <filippob2002@gmail.com>
2024-10-01 20:37:31 -0300

    test array palabra a adivinar + funcionalidades

--- ahorcado-agiles/src/components/ahorcado.js
+++ ahorcado-agiles/src/components/ahorcado.js
@@ -6,7 +6,7 @@ export class Ahorcado {
 
     constructor(palabra_secreta) {
         this.palabra_a_adivinar = palabra_secreta
-        this.array_palabra_a_adivinar=Array(this.palabra_a_adivinar.length).fill("-")
+        this.array_palabra_a_adivinar = Array(this.palabra_a_adivinar.length).fill("-")
     }
 
     adivinar_letra(letra) {
@@ -16,21 +16,31 @@ export class Ahorcado {
         // Si la letra esta en letras, se elimina de letras y continua
         this.letras[this.letras.indexOf(letra)] = "-";
 
-        // Si esta en palabra_secreta, se retorna true
+        // Si esta en palabra_secreta, se retorna true y ocupa las posiciones del array_palabra_a_adivinar
         if (this.palabra_a_adivinar.includes(letra)) {
+            for (let i = 0; i < this.palabra_a_adivinar.length; i++) {
+                if (this.palabra_a_adivinar[i] === letra) {
+                    this.array_palabra_a_adivinar[i] = letra
+                }
+            }
             return true
         } else {
             // Si no esta en palabra_secreta, se resta un intento y se retorna false
             this.intentos_restantes -= 1;
             return false;
-        }   
+        }
     }
 
     adivinar(palabra) {
-        if (palabra !== this.palabra_a_adivinar){
+        if (palabra !== this.palabra_a_adivinar) {
             this.intentos_restantes -= 1;
             return false
-        } else return true
+        } else {
+            for (let i = 0; i < this.palabra_a_adivinar.length; i++) {
+                this.array_palabra_a_adivinar[i] = palabra[i];
+            }
+            return true
+        }
     }
 
 }
--- ahorcado-agiles/src/components/ahorcado.spec.js
+++ ahorcado-agiles/src/components/ahorcado.spec.js
@@ -3,50 +3,62 @@ import { Ahorcado } from "./ahorcado";
 
 const ahc = new Ahorcado("Dia")
 
-test("Palabra a adivinar: Dia, array_palabra_a_adivinar: ['-', '-', '-']", 
+
+test("Palabra a adivinar: Dia, array_palabra_a_adivinar: ['-', '-', '-']",
     () => expect(ahc.array_palabra_a_adivinar).toEqual(['-', '-', '-']));
 test("Adivinar 'Dia'", () => expect(ahc.adivinar("Dia")).toBe(true));
 test("Adivinar 'Dio'", () => expect(ahc.adivinar("Dio")).toBe(false));
 test("Adivinar letra 'a'", () => expect(ahc.adivinar_letra("a")).toBe(true));
 test("Adivinar letra 'b'", () => expect(ahc.adivinar_letra("b")).toBe(false));
-
-
-test("Adivinar letra perteneciente, intentos intactos",()=>{
+test("Adivinar letra perteneciente, intentos intactos", () => {
     let ahorcado = new Ahorcado("palabra")
     ahorcado.adivinar_letra("a")
     expect(ahorcado.intentos_restantes).toBe(7)
 })
-test("Adivinar letra perteneciente dos veces, intentos intactos",()=>{
+test("Adivinar letra perteneciente dos veces, intentos intactos", () => {
     let ahorcado = new Ahorcado("palabra")
     ahorcado.adivinar_letra("a")
     ahorcado.adivinar_letra("a") // Te deja intentar letras que ya adivinaste, pero no te quita intentos
     expect(ahorcado.intentos_restantes).toBe(7)
 })
-
-test("Adivinar letra no perteneciente, intentos -1",()=>{
+test("Adivinar letra no perteneciente, intentos -1", () => {
     let ahorcado = new Ahorcado("palabra")
     ahorcado.adivinar_letra("d")
     expect(ahorcado.intentos_restantes).toBe(6)
 })
-test("Adivinar palabra correctamente, intentos intactos",()=>{
+test("Adivinar palabra correctamente, intentos intactos", () => {
     let ahorcado = new Ahorcado("palabra")
     ahorcado.adivinar("palabra")
     expect(ahorcado.intentos_restantes).toBe(7)
 })
-test("Adivinar palabra incorrectamente, intentos -1",()=>{
+test("Adivinar palabra incorrectamente, intentos -1", () => {
     let ahorcado = new Ahorcado("palabra")
     ahorcado.adivinar("frase")
     expect(ahorcado.intentos_restantes).toBe(6)
 })
-
-test("Adivinar letra perteneciente, letras restantes sin la letra",()=>{
+test("Adivinar letra perteneciente, letras restantes sin la letra", () => {
     let ahorcado = new Ahorcado("palabra")
     ahorcado.adivinar_letra("a")
     expect(ahorcado.letras).not.toContain("a")
 })
-test("Adivinar letra no perteneciente, letras restantes sin la letra e intentos -1",()=>{
+test("Adivinar letra no perteneciente, letras restantes sin la letra e intentos -1", () => {
     let ahorcado = new Ahorcado("palabra")
     ahorcado.adivinar_letra("z")
     expect(ahorcado.letras).not.toContain("z")
     expect(ahorcado.intentos_restantes).toBe(6)
+})
+test("Encontrar una letra en el array de palabra a adivinar", () => {
+    let ahorcado = new Ahorcado("palabra")
+    ahorcado.adivinar_letra("l")
+    expect(ahorcado.array_palabra_a_adivinar).toEqual(['-', '-', 'l', '-', '-', '-', '-'])
+})
+test("Encontrar todas las letras en el array de palabra a adivinar", () => {
+    let ahorcado = new Ahorcado("palabra")
+    ahorcado.adivinar_letra("a")
+    expect(ahorcado.array_palabra_a_adivinar).toEqual(['-', 'a', '-', 'a', '-', '-', 'a'])
+})
+test("Adivinar palabra y rellenar el array de palabra a adivinar", () => {
+    let ahorcado = new Ahorcado("palabra")
+    ahorcado.adivinar("palabra")
+    expect(ahorcado.array_palabra_a_adivinar).toEqual(['p', 'a', 'l', 'a', 'b', 'r', 'a'])
 })
