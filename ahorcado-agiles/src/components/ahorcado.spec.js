7897d3d3e0be57120528a078df47132edc675e79
Ramiro-DG <dia.digiacinti.ramiro@gmail.com>
2024-11-10 10:14:55 -0300

    Added full game tests

--- ahorcado-agiles/src/components/ahorcado.spec.js
+++ ahorcado-agiles/src/components/ahorcado.spec.js
@@ -6,10 +6,13 @@ const ahc = new Ahorcado("Dia")
 
 test("Palabra a adivinar: Dia, array_palabra_a_adivinar: ['-', '-', '-']",
     () => expect(ahc.array_palabra_a_adivinar).toEqual(['-', '-', '-']));
-test("Adivinar 'Dia'", () => expect(ahc.adivinar("Dia")).toBe(true));
-test("Adivinar 'Dio'", () => expect(ahc.adivinar("Dio")).toBe(false));
-test("Adivinar letra 'a'", () => expect(ahc.adivinar_letra("a")).toBe(true));
-test("Adivinar letra 'b'", () => expect(ahc.adivinar_letra("b")).toBe(false));
+
+test("Adivinar 'Dia' y es correcta", () => expect(ahc.adivinar("Dia")).toBe(true));
+test("Adivinar 'Dio' y es incorrecta", () => expect(ahc.adivinar("Dio")).toBe(false));
+
+test("Adivinar letra 'a' y acertando", () => expect(ahc.adivinar_letra("a")).toBe(true));
+test("Adivinar letra 'b' y fallando", () => expect(ahc.adivinar_letra("b")).toBe(false));
+
 test("Adivinar letra perteneciente, intentos intactos", () => {
     let ahorcado = new Ahorcado("palabra")
     ahorcado.adivinar_letra("a")
@@ -61,4 +64,31 @@ test("Adivinar palabra y rellenar el array de palabra a adivinar", () => {
     let ahorcado = new Ahorcado("palabra")
     ahorcado.adivinar("palabra")
     expect(ahorcado.array_palabra_a_adivinar).toEqual(['p', 'a', 'l', 'a', 'b', 'r', 'a'])
+})
+
+test("Juego completo con 3 letras adivinadas, palabra adivinada y gana", () => {
+    let ahorcado = new Ahorcado("palabra")
+    ahorcado.adivinar_letra("a")
+    ahorcado.adivinar_letra("p")
+    ahorcado.adivinar_letra("l")
+    expect(ahorcado.adivinar("palabra")).toBe(true)
+    expect(ahorcado.intentos_restantes).toBe(7)
+    expect(ahorcado.array_palabra_a_adivinar).toStrictEqual(['p', 'a', 'l', 'a', 'b', 'r', 'a'])
+})
+
+test("Juego completo con 3 letras adivinadas, 7 letras no adivinada y pierde", () => {
+    let ahorcado = new Ahorcado("palabra")
+    ahorcado.adivinar_letra("a")
+    ahorcado.adivinar_letra("p")
+    ahorcado.adivinar_letra("l")
+    expect(ahorcado.array_palabra_a_adivinar).toStrictEqual(['p', 'a', 'l', 'a', '-', '-', 'a'])
+
+    ahorcado.adivinar_letra("c")
+    ahorcado.adivinar_letra("d")
+    ahorcado.adivinar_letra("e")
+    ahorcado.adivinar_letra("f")
+    ahorcado.adivinar_letra("g")
+    ahorcado.adivinar_letra("h")
+    ahorcado.adivinar_letra("i")
+    expect(ahorcado.intentos_restantes).toBe(0)
 })
